ff4938d02958890cf7206d6b01d9b9fbafe14769
Syd <enderoverlord@gmail.com>
2020-07-26 18:05:55 -0400

    Update for vs2019

--- PalanoiskaApproach/src/engine/animator.hpp
+++ PalanoiskaApproach/src/engine/animator.hpp
@@ -5,18 +5,18 @@
 #include <vector>
 
 //Player animation states
-enum AnimationStates { left, right };
+enum class AnimationStates { left, right };
 
 struct Animation {
 	std::vector<sf::Texture*> frames;
-	int speed;
+	int speed = 0;
 	int timer = 0; //Counts updates till next frame
 	unsigned int frame = 0;
 };
 
 class PlayerAnimator {
 public:
-	PlayerAnimator() {}
+	PlayerAnimator() { currentAnimation = AnimationStates::right; }
 	PlayerAnimator(Animation left, Animation right);
 	void update(const float hVelocity, sf::Sprite& sprite);
 	sf::Texture* getCurrentFrame();
